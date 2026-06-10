ca4dea28edb381f4e79f68cb0e31262b518c226c
khanmila95-alt <khanmila95@gmail.com>
2026-06-10 22:51:52 +0600

    Add Professor class extending AcademicStaff

---
+++ Professor.java
@@ -0,0 +1,24 @@
+class Professor extends AcademicStaff {
+    private String researchArea;
+    private int publications;
+
+    public Professor(int memberId, String name,
+                     double salary, String department,
+                     String researchArea, int publications) {
+
+        super(memberId, name, salary, department);
+        this.researchArea = researchArea;
+        this.publications = publications;
+    }
+
+    public void showResearchProfile() {
+        System.out.println("Research Area: " + researchArea);
+        System.out.println("Publications: " + publications);
+    }
+
+    @Override
+    public void performDuty() {
+        System.out.println(name +
+                " conducts research and teaches advanced courses.");
+    }
+}
